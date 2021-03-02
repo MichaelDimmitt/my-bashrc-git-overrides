ca5698be65f20ae25bf176b3ed99f88d67e380a6
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-03-02 09:07:56 -0500

    Create git-overrides.sh

---
+++ git-overrides.sh
@@ -0,0 +1,12 @@
+git() {
+  if [ "$1" == "check" ]; then
+    bash <(curl -s https://raw.githubusercontent.com/MichaelDimmitt/git_check_computer/master/git_check_computer.sh)
+  elif [ "$1" == "add" ]; then
+    $(which git) "$@"
+    while IFS='' read -r LINE || [ -n "${LINE}" ]; do
+      $(which git) reset HEAD $LINE;
+    done < .hide_tracked
+  else
+    $(which git) "$@"
+  fi
+}
